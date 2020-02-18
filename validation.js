21ae7ff57cc5460c2d244575c3eac67af9883493
heavy58 <60599713+heavy58@users.noreply.github.com>
2020-02-18 14:40:27 +0100

    first commit

---
+++ validation.js
@@ -0,0 +1,33 @@
+// console.log('Start')
+// let button = document.getElementById('button')
+// console.log(button)
+// let name = document.getElementById('name')
+// console.log(name)
+// console.log("Hello")
+// let ad = document.getElementById('adr')
+// let email = document.getElementById('eadr')
+// let password = document.getElementById('pass')
+// let comme = document.getElementById('comm')
+// button.addEventListener("click", (e)=> {
+//      e.preventDefault()
+//      popup1(name,ad,email,password,comme)
+//      popup2(password)
+
+// })
+
+
+// function popup1(name,ad,email,password,comme) {
+//     console.log("name")
+   
+//     if (name.value === "" || ad.value === "" || email.value === "" || password.value === "" || comme.value === "") {
+//         alert('champ vide');
+//     }
+// }
+// function popup2(pass) {
+    
+//     if (pass.value.length < 8) {
+//         alert('the password has less than 8 characters')
+
+
+//     }
+// }
